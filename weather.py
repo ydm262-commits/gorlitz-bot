4f318f08829f78ec5bcf6e6d478ef2accd060523
ydm262-commits <ydm262@gmail.com>
2026-04-26 13:57:30 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
 class WeatherClient:
     @staticmethod
-        async def get_friday_forecast():
-                return {'is_rainy': False, 'description_he': ''}
+    async def get_friday_forecast():
+        return {'is_rainy': False, 'description_he': ''}
